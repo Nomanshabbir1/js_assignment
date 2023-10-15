e3fb2bafa9ddee388f1296cf16aff2f8b120e642
Nomanshabbir1 <135643492+Nomanshabbir1@users.noreply.github.com>
2023-10-15 21:28:46 +0500

    html, css, js file added

---
+++ app.js
@@ -0,0 +1,23 @@
+var data = document.getelementbyid('alldata');
+var alldata = {
+    Toyota: {
+        cars: {
+            toyotaCorolla: {
+                name: "Toyota Corolla",
+                model: "2023",
+                color: "white",
+                cityName: "Karachi",
+                Price: "PKR 2,80,0000"
+            },
+            ToyotaCamry:{name: "Toyota Corolla",
+            model: "2023",
+            color: "white",
+            cityName: "Karachi",
+            Price: "PKR 2,80,0000"
+
+            }
+
+        }
+    }
+
+}
